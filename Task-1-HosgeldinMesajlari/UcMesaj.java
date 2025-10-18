f9303973532be2263b325856ff1a2b9e46fdb790
Halit1elnehsen <halitelnehsen76@gmail.com>
2025-10-18 20:42:25 +0300

    UcMesaj.java

---
+++ Task-1-HosgeldinMesajlari/UcMesaj.java
@@ -0,0 +1,12 @@
+Halid Elnehsen
+  250541617
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class UcMesaj {
+    public static void main(String[] args) {
+        System.out.println("Java'ya Hos Geldiniz");
+        System.out.println("Bilgisayar Bilimine Hos Geldiniz");
+        System.out.println("Programlama Eglencelidir");
+    }
+}
