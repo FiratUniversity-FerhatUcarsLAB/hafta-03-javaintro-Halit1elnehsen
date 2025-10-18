97af22dc7ad4906540d3cf8437ad91940df1bd3e
Halit1elnehsen <halitelnehsen76@gmail.com>
2025-10-18 20:14:09 +0300

    BesKereTekrar.java

---
+++ Task-1-HosgeldinMesajlari/BesKereTekrar.java
@@ -0,0 +1,14 @@
+Halid Elnehsen 
+  250541617
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class ödev {
+    public static void main(String[] args) {
+        System.out.println("Java'ya hos geldiniz");
+        System.out.println("Java'ya hos geldiniz");
+        System.out.println("Java'ya hos geldiniz");
+        System.out.println("Java'ya hos geldiniz");
+        System.out.println("Java'ya hos geldiniz");
+    }
+}
