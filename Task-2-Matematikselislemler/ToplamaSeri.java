7a1f18c6c71ed4ab9e1145bda3111626872dab4d
Halit1elnehsen <halitelnehsen76@gmail.com>
2025-10-19 13:06:06 +0300

    ToplamaSeri.java

--- Task-2-Matematikselislemler/ToplamaSeri.java
+++ Task-2-Matematikselislemler/ToplamaSeri.java
@@ -1,12 +1,1 @@
-Halid Elnehsen
-  250541617
-// Online Java Compiler
-// Use this editor to write, compile and run your Java code online
-class Main {
-    public static void main(String[] args) {
-    double pay = (9.5*-3.5)-(2.5*3);
-    double payda = 45.5-3.5;
-    double sonuc = pay/payda;
-        System.out.printf("%.4f\n",sonuc);
-    }
-}
+
