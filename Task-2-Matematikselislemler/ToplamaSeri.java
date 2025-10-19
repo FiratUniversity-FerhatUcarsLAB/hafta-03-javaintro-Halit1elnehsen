534a4a1d7b545df7a5782a6839ec3cf266a1347c
Halit1elnehsen <halitelnehsen76@gmail.com>
2025-10-19 13:26:03 +0300

    ToplamaSeri.java

--- Task-2-Matematikselislemler/ToplamaSeri.java
+++ Task-2-Matematikselislemler/ToplamaSeri.java
@@ -1,1 +1,10 @@
+Halid Elnehsen
+  250541617
+public class ToplamaSeri {
+
+    public static void main(String[] args) {
+        int sonuc =  1+2+3+4+5+6+7+8+9;  
+           System.out.println( "1+2+3+4+5+6+7+8+9="+sonuc);
+    }
+}
 
