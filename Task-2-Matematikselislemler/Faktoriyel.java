518f337457846c206536514a1a175c73ea63fe25
Halit1elnehsen <halitelnehsen76@gmail.com>
2025-10-20 16:50:57 +0300

    Faktoriyel.java

--- Task-2-Matematikselislemler/Faktoriyel.java
+++ Task-2-Matematikselislemler/Faktoriyel.java
@@ -1,10 +1,9 @@
-Halid Elnehsen
-  250541617
-// Online Java Compiler
-// Use this editor to write, compile and run your Java code online
+//Halid Elnehsen
+ // 250541617
 class Main {
     public static void main(String[] args) {
         int sonuc = 5 * 4 * 3 * 2 * 1;
         System.out.println("5! = 5 × 4 × 3 × 2 × 1 = " + sonuc);
     }
 }
+
