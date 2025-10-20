dc0d44ec08ba84b084fd416ee751c350a4b4d181
Halit1elnehsen <halitelnehsen76@gmail.com>
2025-10-20 19:20:26 +0300

    UcgenAlanHesap.java

---
+++ Task-3-GeometrikHesaplamalar/UcgenAlanHesap.java
@@ -0,0 +1,18 @@
+package ucgenhesaplam;
+
+
+public class UcgenHesaplam {
+
+    
+    public static void main(String[] args) {
+         double a = 3;
+        double b = 4;
+        double c = 5;
+
+        double s = (a + b + c) / 2;
+        double alan = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+
+        System.out.println("ucgenin Alani: " + alan);
+    }
+    
+}
