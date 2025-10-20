295b95c0f8b75d4437e46d3ebe6bc76fa3a28dd9
Halit1elnehsen <halitelnehsen76@gmail.com>
2025-10-20 20:18:17 +0300

    UsluSayilarTablosu.java

---
+++ Task-5-TabloDesenleri/UsluSayilarTablosu.java
@@ -0,0 +1,12 @@
+public class UsluSayilarTablosu {
+    public static void main(String[] args) {
+        System.out.println("a\t a^2\t a^3");
+        System.out.println("-----------------");
+
+        for (int a = 1; a <= 5; a++) {
+            int karesi = a * a;
+            int kupu = a * a * a;
+            System.out.println(a + "\t " + karesi + "\t " + kupu);
+        }
+    }
+}
