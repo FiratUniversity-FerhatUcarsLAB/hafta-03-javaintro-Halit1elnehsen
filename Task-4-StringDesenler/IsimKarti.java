3832820294dd636f038a36b1f0a1e18f7eefcff1
Halit1elnehsen <halitelnehsen76@gmail.com>
2025-10-20 19:41:13 +0300

    IsimKarti.java

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,17 @@
+public class IsimKarti {
+    public static void main(String[] args) {
+        String ad = "Halid";
+        String soyad = "Elnehhsen";
+        String ogrenciNo = "202501234";
+        String bolum = "Bilgisayar Mühendisliği";
+
+        System.out.println("+--------------------------------------+");
+        System.out.println("|              İSİM KARTI              |");
+        System.out.println("+--------------------------------------+");
+        System.out.println("| Adı         : " + ad);
+        System.out.println("| Soyadı      : " + soyad);
+        System.out.println("| Öğrenci No  : " + ogrenciNo);
+        System.out.println("| Bölüm       : " + bolum);
+        System.out.println("+--------------------------------------+");
+    }
+}
