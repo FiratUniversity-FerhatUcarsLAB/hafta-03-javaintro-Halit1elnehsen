963b5ed7a734231cd294c403d5521303f60c3e9b
Halit1elnehsen <halitelnehsen76@gmail.com>
2025-10-20 18:54:42 +0300

    DikdortgenHesap.java

---
+++ Task-3-GeometrikHesaplamalar/DikdortgenHesap.java
@@ -0,0 +1,12 @@
+public class DikdortgenHesaplama {
+    public static void main(String[] args) {
+        double en = 4.5;
+        double boy = 7.9;
+
+        double alan = en * boy;
+        double cevre = 2 * (en + boy);
+
+        System.out.println("Dikdörtgenin Alanı: " + alan);
+        System.out.println("Dikdörtgenin Çevresi: " + cevre);
+    }
+}
